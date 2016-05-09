4685d926da98276e2cfd6524b15327c5f0b761f5
vvictoria <viniarska.v@gmail.com>
2016-05-10 01:35:27 +0300

    added languages

--- js/main.js
+++ js/main.js
@@ -13,4 +13,37 @@ window.addEventListener('load', function () {
             }
         }
     });
+
+    $("#show-menu").click(function(e) {
+        e.preventDefault();
+        $(".menu").toggleClass('visible');
+    });
+
+
+
+    $('.menu a').click(function(e) {
+        $(".menu").removeClass("visible");
+    });
+
+
+    if(!localStorage.rememberLanguage) {
+        window.localStorage.rememberLanguage = "en";
+    }
+    var i18n = domI18n({
+        languages: ['en', 'uk'],
+        currentLanguage: window.localStorage.rememberLanguage
+
+    });
+    $(".uk").click(function (e) {
+        e.preventDefault();
+        i18n.changeLanguage('uk');
+        window.localStorage.rememberLanguage = "uk";
+    });
+    $(".en").click(function (e) {
+        e.preventDefault();
+        i18n.changeLanguage('en');
+        window.localStorage.rememberLanguage = "en";
+    });
+
+
 });
